bfba1da5ae603aa043a9c948afabf70b6d8360c3
David-Madilyan <David.Madilyan@south.rt.ru>
2022-01-31 00:10:57 +0300

    доблено диалоговое окно и снэкбар

--- lib/detail_quiz/detail_controller.dart
+++ lib/detail_quiz/detail_controller.dart
@@ -1,16 +1,20 @@
 import 'dart:math';
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:get/get.dart';
 import 'package:poll_answer/api/api_response.dart';
 import 'package:poll_answer/api/api_rest.dart';
 import 'package:poll_answer/model/answer.dart';
 import 'package:poll_answer/model/question.dart';
 import 'package:poll_answer/navigation/routes.dart';
+import 'package:poll_answer/theme/colors.dart';
+import 'package:poll_answer/widgets/snackbar.dart';
 
 class DetailController extends GetxController {
   int allVotedCount = 0;
   Question? question;
   Rx<Status> statusVoted = Status.Empty.obs;
+  bool isShowedSnackBar = false;
   @override
   void onReady() {}
 
@@ -21,7 +25,7 @@ class DetailController extends GetxController {
 
   void onSummaryVoted(List<Answer>? answerVariants) {
     answerVariants?.forEach((element) {
-      element.votedCount = Random().nextInt(1000);
+      // element.votedCount = Random().nextInt(1000);
       allVotedCount += element.votedCount;
     });
   }
@@ -42,9 +46,60 @@ class DetailController extends GetxController {
     if (question == null) statusVoted.value = Status.Error;
     var response = await RestApi.voteByQuestion(question?.id ?? 0, id);
     statusVoted.value = response.status;
+    _showSnackbar(statusVoted.value);
   }
 
   void navigateToInfo() {
     Get.toNamed(Routes.INFO);
+  }
+
+  void _showSnackbar(Status status) {
+    if (isShowedSnackBar) return;
+    if (status == Status.Success) {
+      showSnackbar(
+        tr('success_title'),
+        "assets/img/ic_ok.png",
+        iconColorType2,
+        false,
+      );
+    } else if (status == Status.Error) {
+      showSnackbar(
+        tr('error_request'),
+        "assets/img/ic_err.png",
+        iconColorType3,
+        true,
+      );
+    } else {
+      showSnackbar(
+        tr('double_vote_error'),
+        "assets/img/ic_err.png",
+        iconColorType3,
+        true,
+      );
+    }
+
+    isShowedSnackBar = true;
+    // dialogAction("Success", [
+    //   DialogActionParams(
+    //     text: tr('show_stat'),
+    //     gradient: LinearGradient(colors: [
+    //       Color(0xFF5DAC6E),
+    //       Color(0xFF00E08F),
+    //     ]),
+    //     imagePath: "assets/img/ic_stat.png",
+    //     overlayColor: Color(0xB24B8F5A),
+    //     action: null,
+    //   ),
+    //   DialogActionParams(
+    //     text: tr('close_quiz'),
+    //     gradient: LinearGradient(colors: [
+    //       Color(0xFF5DAC6E),
+    //       Color(0xFF00E08F),
+    //     ]),
+    //     imagePath: "assets/img/ic_edit.png",
+    //     overlayColor: Color(0xB24B8F5A),
+    //     action: null,
+    //   ),
+    // ]);
   }
 }
